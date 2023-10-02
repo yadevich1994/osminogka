97c3090f18f213fb2d60605c613914c1f2ec889b
Yadevich94 <yadevich94@gmail.com>
2023-10-02 16:52:24 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'header-img': "url('/images/header.jpg')",
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
